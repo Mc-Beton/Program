bcdac44ea876f685329871acbb1b4a6677eea75c
Filip Gabrys <el.filberto@gmail.com>
2021-02-22 15:31:33 +0100

    Update forms

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,10 @@
 from werkzeug.routing import ValidationError
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, TextAreaField, BooleanField, FileField
+from wtforms import  StringField, PasswordField, TextAreaField, BooleanField, FileField
 from wtforms.validators import DataRequired
 from config import Config
+from app.models import Photo, Cross, Material, Lampion, Wazon, Podst, Letters
+from wtforms_sqlalchemy.fields import QuerySelectField
 
 class LoginForm(FlaskForm):
 
@@ -19,6 +21,28 @@ class LoginForm(FlaskForm):
             raise ValidationError("Invalid password")
         return field.data
 
+def photo_query():
+   return Photo.query
+
+def cross_query():
+   return Cross.query
+
+def mat_query():
+    return Material.query
+
+def let_query():
+    return Letters.query
+
+def wazon_query():
+    return Wazon.query
+
+def podst_query():
+    return Podst.query
+
+def lampion_query():
+    return Lampion.query
+
+
 class ClientForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     surname = StringField('Surname', validators=[DataRequired()])
@@ -26,43 +50,46 @@ class ClientForm(FlaskForm):
     tel2 = StringField('Tel2')
     loc = StringField('Loc')
     info = TextAreaField('Info')
+    zal = StringField('zaliczka')
+    pri = StringField('Cena')
 
-class OrderForm(FlaskForm):
+class OrdersForm(FlaskForm):
     info = TextAreaField('Info')
 
 class ProductForm(FlaskForm):
     kind = StringField('Kind', validators=[DataRequired()])
     finish = StringField('Finish', validators=[DataRequired()])
     parapet = StringField('Parapet', validators=[DataRequired()])
-    mat1 = StringField('Mat1', validators=[DataRequired()])
+    mat1 = QuerySelectField(query_factory=mat_query, allow_blank=True, get_label='name')
     cokoly = StringField('Cokoly', validators=[DataRequired()])
-    mat2 = StringField('Mat2', validators=[DataRequired()])
+    mat2 = QuerySelectField(query_factory=mat_query, allow_blank=True, get_label='name')
     plyta = StringField('Plyta', validators=[DataRequired()])
     thick = StringField('Thick', validators=[DataRequired()])
-    mat3 = StringField('Mat3', validators=[DataRequired()])
-    info = TextAreaField('Info')
+    mat3 = QuerySelectField(query_factory=mat_query, allow_blank=True, get_label='name')
+    info11 = TextAreaField('Info')
     
 class AddsForm(FlaskForm):
-    wazon = BooleanField('Wazon')
-    info = TextAreaField('Info')
-    podstawa = BooleanField('Podstawa')
-    info = TextAreaField('Info')
-    lampion = BooleanField('Lampion')
-    info = TextAreaField('Info')
-    photo = BooleanField('Photo')
-    info = TextAreaField('Info')
-    cross = BooleanField('Cross')
-    info = TextAreaField('Info')
+    wazon = QuerySelectField(query_factory=wazon_query, allow_blank=True, get_label='name')
+    info1 = TextAreaField('Info')
+    podstawa = QuerySelectField(query_factory=podst_query, allow_blank=True, get_label='name')
+    info2 = TextAreaField('Info')
+    lampion = QuerySelectField(query_factory=lampion_query, allow_blank=True, get_label='name')
+    info3 = TextAreaField('Info')
+    photo = QuerySelectField(query_factory=photo_query, allow_blank=True, get_label='name')
+    info4 = TextAreaField('Info')
+    cross = QuerySelectField(query_factory=cross_query, allow_blank=True, get_label='name')
+    info5 = TextAreaField('Info')
 
 class PathForm(FlaskForm):
-    kind = TextAreaField('Kind')
+    kind2 = TextAreaField('Kind')
     border = TextAreaField('Border')
-    info = TextAreaField('Info')
+    bench = TextAreaField('Bench')
+    info8 = TextAreaField('Info')
 
 class WritingForm(FlaskForm):
     content = TextAreaField('Content')
-    kind = TextAreaField('Kind')
-    info = TextAreaField('Info')
+    kind3 = QuerySelectField(query_factory=let_query, allow_blank=True, get_label='name')
+    info7 = TextAreaField('Info')
 
 class MaterialForm(FlaskForm):
     name = TextAreaField('Name', validators=[DataRequired()])
@@ -106,7 +133,8 @@ class LampionForm(FlaskForm):
     photo = TextAreaField('Photo')
 
 class HeadForm(FlaskForm):
-    thick = StringField('Thick', validators=[DataRequired()])
-    mat1 = StringField('mat1')
-    mat2 = StringField('mat2')
-    info = TextAreaField('Info')
+    dim1 = StringField('dim1')
+    mat4 = QuerySelectField(query_factory=mat_query, allow_blank=True, get_label='name')
+    dim2 = StringField('dim2')
+    mat5 = QuerySelectField(query_factory=mat_query, allow_blank=True, get_label='name')
+    info6 = TextAreaField('Info')
